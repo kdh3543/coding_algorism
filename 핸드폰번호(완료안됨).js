7a710e7a0c19921fb9329a6ea208af80d57043c1
kdh3543 <https://github.com/kdh3543>
2022-10-24 16:48:15 +0900

    no message

---
+++ 핸드폰번호(완료안됨).js
@@ -0,0 +1,7 @@
+function solution(phone_number) {
+  return phone_number.replace(phone_number.substring(0, phone_number.length - 4),'*');
+}
+
+const key = '0102345'
+const test =key.replace(/' '/,'*')
+console.log(test)
--- 핸드폰번호.js
+++
@@ -1,4 +0,0 @@
-function solution(phone_number) {
-  return phone_number.split('').fill('*',0,phone_number.length-4).join('')
-}
-
