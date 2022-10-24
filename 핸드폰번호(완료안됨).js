e20ec8270f3981e932f1581f4aa53832f21845f1
kdh3543 <https://github.com/kdh3543>
2022-10-24 18:56:31 +0900

    no message

--- 핸드폰번호(완료안됨).js
+++
@@ -1,7 +0,0 @@
-function solution(phone_number) {
-  return phone_number.replace(phone_number.substring(0, phone_number.length - 4),'*');
-}
-
-const key = '0102345'
-const test =key.replace(/' '/,'*')
-console.log(test)
