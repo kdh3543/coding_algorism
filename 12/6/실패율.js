6dbc6bc447200362476094ef7198a21eaa093cd3
kdh3543 <https://github.com/kdh3543>
2022-12-09 12:49:21 +0900

    12/9 study

--- 12/6/실패율.js
+++ 12/6/실패율.js
@@ -1,4 +1,26 @@
 function solution(N, stages) {
-  var answer = [];
+  let answer = []
+  let result = []
+  let number = 0
+  
+  for (let i = 0; i < N; i++){
+    number = 0
+    stages.map((value) => {
+      if (value === i + 1) {
+        number++
+      }
+    })
+    result.push(number)
+  }
+  let stagesLength = stages.length
+  let realResult = []
+  for (let i = 0; i < N; i++){
+
+    realResult.push(result[i] / stagesLength)
+    stagesLength = stagesLength - result[i]
+  }
+  realResult.map((value, index) => [value, index + 1]).sort((a, b) => b[0] - a[0]).map((value) => answer.push(value[1]))
   return answer;
 }
+
+solution(5,[2,1,2,6,2,4,3,3])
