0b8bf9b72248694bebf9ae6b16d4a80b49604703
김동현 <rlaehdgus3543@naver.com>
2023-04-14 11:33:13 +0900

    4/14 study 진행중

--- LeetCode/4/14/LCPrefix.js
+++ LeetCode/4/14/LCPrefix.js
@@ -3,16 +3,31 @@
  * @return {string}
  */
 var longestCommonPrefix = function (strs) {}
-
-let strs = ['flower', 'flow', 'flight']
-
+// 연속된 문자열이 맞아야 됨
+let strs =["cir","car"]
 let arr = strs.map((val) => val.split(''))
 console.log(arr)
 
+let answer = ''
 
-while(strs.length!==0){
-  let word = strs.shift()
-
-  console.log(word)
+if(strs.length < 2) {
+  answer = strs.join('')
+  return answer
+}else{
+  while(true){
+    let word = strs.shift()
+    if(strs.length === 0)break
   
+    let common = strs[0].split('').filter(v=>word.split('').includes(v))
+    if(common.length===0){
+      answer = ''
+      break;
+    }
+    answer = common
+  }
+  console.log(answer.length === 0 ? answer: answer.join(''))
+  return
 }
+
+
+console.log('answer',answer)
