655169d6fe632064793f9ef7a577a62d69b7d035
Donghyun Kim <91539013+kdh3543@users.noreply.github.com.>
2024-07-05 14:12:51 +0900

    24/07/05

--- 백준/2024.07/5/빠른AB.js
+++ 백준/2024.07/5/빠른AB.js
@@ -2,10 +2,11 @@ const fs = require("fs");
 const file = process.platform === "linux" ? "dev/stdin" : "example.txt";
 let input = fs.readFileSync(file).toString().trim().split("\n");
 
+const a = parseInt(input.shift().trim());
 let str = "";
-for (let i = 0; i < input.length; i++) {
-  const elInput = input[i].trim().split(" ");
-  str += parseInt(elInput[0]) + parseInt(elInput[1]) + "\n";
+for (let i = 0; i < a; i++) {
+  const inputSplit = input[i].trim().split(" ");
+  str += parseInt(inputSplit[0]) + parseInt(inputSplit[1]) + "\n";
 }
 
 console.log(str);
