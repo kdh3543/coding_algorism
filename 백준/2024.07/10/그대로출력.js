6dd47037464f3519d7b6d58f4db3769b373b7a2e
Donghyun Kim <91539013+kdh3543@users.noreply.github.com.>
2024-07-10 17:07:12 +0900

    24/07/10

--- 백준/2024.07/10/그대로출력.js
+++ 백준/2024.07/10/그대로출력.js
@@ -7,8 +7,4 @@ const input = fs
   .split("\n")
   .map((v) => v.trim());
 
-const N = parseInt(input.shift());
-
-for (let i = 0; i < N; i++) {
-  console.log(input[i].charAt(0) + input[i].charAt(input[i].length - 1));
-}
+input.forEach((v) => console.log(v));
