5295991b4a1543d8d103ffa414a3086201fe28fa
Donghyun Kim <91539013+kdh3543@users.noreply.github.com.>
2024-07-23 17:54:56 +0900

    24/07/23

--- 백준/index.js
+++ 백준/index.js
@@ -5,16 +5,8 @@ let input = fs
   .toString()
   .trim()
   .split("\n")
-  .map((v) => v.trim().split(""));
+  .map((v) => v.trim());
 
-let resultArr = [];
-
-for (let i = 0; i < input.length; i++) {
-  for (let j = 0; j < input[i].length; j++) {
-    if (input[i][j]) {
-      resultArr[j] = resultArr[j] ? resultArr[j] + input[i][j] : input[i][j];
-    }
-  }
-}
-
-console.log(resultArr.join(""));
+const N = parseInt(input.shift());
+let result = N * 100;
+console.log(input);
